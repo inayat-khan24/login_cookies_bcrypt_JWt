3a3ee7d2c3ffadafafe6c9e4d7317389409c85bb
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-30 01:26:26 +0530

    we set and use of cookies

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,21 +1,35 @@
 export const getRegisterPage = (req, res) => {
+  // acces login file form views/auth/register
   res.render("auth/register");
 };
 
 export const getLoginPage = (req, res) => {
-  // login rout
+  // acces login file form views/auth/login
   res.render("auth/login");
 };
 
 export const getHomePage = (req,res)=>{
+// for access views ejs file
+let isLoggedIn =  req.headers.cookie;
+    // first we break the base on ";" to converto into an  array 
+     // after that we remove whites space with the help of trim and
+     // we use startswith to find first element of isLoggedIn value in the array.
+    isLoggedIn = Boolean(isLoggedIn?.split(";")?.find((cookie)=>cookie.trim()
+    .startsWith(("isLoggedIn")))?.split("=")[1])
+    
+    // now we gave this value to index.ejs file 
+    return res.render("index",{isLoggedIn})
 
-res.render("index");
+
 }
 
 // post 
 export const postLogin = (req,res)=>{
-   // set cookies
-   res.setHeader("Set-Cookie","isLoggedIn=true; path=/;")
+   // set cookies and if we set path="/" like that then cookie take true every page
+   // that's why we shuld give like that path="/" 
+   res.setHeader("Set-Cookie","isLoggedIn=true; path=/")
+
+    
     
    // if we will click login button then the page move to home page 
   res.redirect("/")
