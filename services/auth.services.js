350efafe77c9672e92a1c695d17abce52ce00109
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-30 18:23:31 +0530

    compelte with argon2

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,4 +1,5 @@
-import bcrypt  from "bcrypt"
+// import bcrypt  from "bcrypt"
+import argon2 from "argon2"
 
 
 
@@ -7,13 +8,18 @@ import bcrypt  from "bcrypt"
 export const hashPassword = async (password)=>{
      //  first we hashing password with bcrypt and and we get password form uthcontrollers  
     //   second we use salt to porvid extra complex number like 10
-return  await bcrypt.hash(password,10)
+// return  await bcrypt.hash(password,10)
+return  await argon2.hash(password)
 }
 
 
 // we will compare use pass and which password shore in database they compare both
 export const comparePassword= async(password,hashpassword)=>{
- return await bcrypt.compare(password,hashpassword)
+  //  bcrypt syntext  bcrypt.compare(plainText,hashpassword)
+//  return await bcrypt.compare(password,hashpassword)
+  //  argon2 syntext  argon2.verify(hashpassword,plainText)
+  return await argon2.verify(hashpassword,password)
+
 }
 
 
